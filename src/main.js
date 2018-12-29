0e383146f213f8e8bb6217d063a1993fb1f893a3
Yuumi <yuuming.n@gmail.com>
2018-12-29 23:52:25 +0900

    Add save the data for local function

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
